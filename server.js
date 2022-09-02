18c4776d29d4c8bd4b8283aa10bda2e7d070ed67
totxprex <totxprex@gmail.com>
2022-09-02 01:48:11 +0100

    noew

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors({
 app.use(express.json())
 
 let helmet = require('helmet')
+app.use(helmet())
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }))
 
 let dotenv = require('dotenv')
